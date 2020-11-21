f66ac8c5783b9a3b219355eae1547244d9098ddf
alejandro silva <alejandro.silva.m@gmail.com>
2020-11-21 20:31:57 -0300

    cambio express json

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ app.set("port", process.env.PORT || 3000);
 
 // middlewares
 app.use(morgan("dev"));
-//app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
 
 // routes
 app.use(require("./routes/index"));
