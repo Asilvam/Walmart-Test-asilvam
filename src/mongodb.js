942609b9a6fe38f2e664145e4fb2d3eb39046be4
asilvam <alejandro.silva.m@gmail.com>
2022-02-11 12:53:02 -0300

    mejoras varias

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
-// const URI1 ='mongodb+srv://root:admin@cluster0-jim6x.mongodb.net/test?retryWrites=true&w=majority';
- const URI3 ="mongodb+srv://admin:Domi2702@cluster0.gpflb.mongodb.net/test_w?retryWrites=true&w=majority"
-//const URI ="mongodb://root:admin@cluster0-shard-00-00-jim6x.mongodb.net:27017,cluster0-shard-00-01-jim6x.mongodb.net:27017,cluster0-shard-00-02-jim6x.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
-//const URI2 = "mongodb://mongo/mydatabase"
+//const URI ='mongodb+srv://root:admin@cluster0-jim6x.mongodb.net/test?retryWrites=true&w=majority';
+const URI = "mongodb://mongo/test"
 mongoose
-  .connect(URI3, {
+  .connect(URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
